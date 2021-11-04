3a4c4cec0aa812b030ceb24595354e0ae72a9447
Kyle <kliang1194@gmail.com>
2021-11-04 11:07:45 -0400

    fix return placement

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,6 +5,7 @@ const fetchBreedDescription = function(breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search/?q=${breedName}`, (error, response, body) => {
     if (error) {
       callback(error, null);
+      return;
     } else {
       const data = JSON.parse(body);
       if (data[0]) {
